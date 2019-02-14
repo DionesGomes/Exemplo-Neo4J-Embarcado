2a4e5132390eb79f669f6615c8f12aa6f943faa6
paulo <paulo.freitas.nt@gmail.com>
2019-02-14 17:15:18 -0300

    acrescentando outros exemplos

---
+++ src/main/java/com/ifpb/neo4j/enumeration/Relacionamentos.java
@@ -0,0 +1,9 @@
+package com.ifpb.neo4j.enumeration;
+
+import org.neo4j.graphdb.RelationshipType;
+
+public enum Relacionamentos implements RelationshipType {
+
+    AMIGO;
+
+}
--- src/main/java/com/ifpb/neo4j/visao/App.java
+++ src/main/java/com/ifpb/neo4j/visao/App.java
@@ -1,12 +1,16 @@
 package com.ifpb.neo4j.visao;
 
 
+import com.ifpb.neo4j.enumeration.Relacionamentos;
 import com.ifpb.neo4j.modelo.Pessoa;
 import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 import java.io.File;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 
 public class App {
@@ -23,10 +27,54 @@ public class App {
 //
 //        exemploSalvarPessoa(graph, pessoa1);
 //        exemploSalvarPessoa(graph, pessoa2);
-
         exemploListarPessoas(graph);
+//        exemploCriarAmizade(graph);
+//        exemploPercorrerAmizade(graph);
+
+//        exemploUsandoCypher(graph);
 
         graph.shutdown();
+
+    }
+
+    private static void exemploUsandoCypher(GraphDatabaseService graph) {
+        try(Transaction tx = graph.beginTx()){
+
+            Map<String, Object> mapa = new HashMap<>();
+            mapa.put("cpf", "111.111.111-01");
+
+            Result result = graph
+                    .execute("MATCH (p:Pessoa) WHERE p.cpf = $cpf RETURN p",
+                            mapa);
+
+            System.out.println(result.resultAsString());
+
+        }
+    }
+
+    private static void exemploPercorrerAmizade(GraphDatabaseService graph) {
+        try(Transaction tx = graph.beginTx()){
+            Node no1 = graph
+                    .findNode(Label.label("Pessoa"), "cpf","111.111.111-01");
+
+            no1.getRelationships(Relacionamentos.AMIGO, Direction.BOTH)
+                    .forEach(r-> System.out.println(r.getProperty("desde")));
+        }
+    }
+
+    private static void exemploCriarAmizade(GraphDatabaseService graph) {
+        try (Transaction tx = graph.beginTx()){
+            Node no1 = graph
+                    .findNode(Label.label("Pessoa"), "cpf", "111.111.111-01");
+            Node no2 = graph
+                    .findNode(Label.label("Pessoa"), "cpf", "222.222.222-02");
+
+            Relationship rel = no1
+                    .createRelationshipTo(no2, Relacionamentos.AMIGO);
+            rel.setProperty("desde", LocalDate.now());
+
+            tx.success();
+        }
 
     }
 
